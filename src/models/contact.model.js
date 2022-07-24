033bf15fcee04699df19f10800c97333792015d3
Jethro Estrada <jethro@macantacrm.com>
2022-07-24 22:17:42 +0800

    Working create contact and address

--- src/models/contact.model.js
+++ src/models/contact.model.js
@@ -4,7 +4,6 @@ module.exports = (sequelize, DataTypes) => {
   class Contact extends Model {
     static associate(models) {
       Contact.Address = Contact.belongsTo(models.Address, {
-        as: 'address',
         foreignKey: 'addressId',
       });
     }
