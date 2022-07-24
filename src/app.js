6975e6e54bd5437ec026993b2f5f62f851c05de2
Jethro Estrada <jethro@macantacrm.com>
2022-07-25 04:38:36 +0800

    Updated express configurations

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors());
+app.options('*', cors());
 
 app.use('/', routes);
 
--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,11 @@ dotenv.config({path: path.join(__dirname, '../../.env')});
 const logger = require('./logger');
 
 const env = process.env.NODE_ENV || 'development';
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3001;
 
 logger.info(`ENVIRONMENT: ${env}`);
 logger.info(`PORT: ${port}`);
+logger.info(`DB URL: ${process.env.DATABASE_URL}`);
 
 module.exports = {
   env,
